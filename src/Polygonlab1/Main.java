4a6af8bf00e8c0a34c4ef56135f0c5cde94a9f25
“nkusikevin” <nkusikvnhart@gmail.com>
2021-11-10 23:43:18 +0200

    final algorithm

--- src/Polygonlab1/Main.java
+++ src/Polygonlab1/Main.java
@@ -3,6 +3,48 @@ package Polygonlab1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int [] intArray = {20,35,-15,7,55,1,-22};
+
+    quickSort(intArray , 0 ,intArray.length);
+
+    for (int i = 0 ; i<intArray.length ; i++ ){
+        System.out.println(intArray[i]);
     }
+
+
+    }
+
+    public static void quickSort(int [] input , int start ,int end){
+        if (end - start < 2){
+            return;
+        }
+        int pivotIndex = partion(input,start,end);
+        quickSort(input ,start,pivotIndex);
+        quickSort(input,pivotIndex+1 ,end);
+    }
+
+    public  static int partion( int [] input , int start ,int end){
+        int pivot = input[start];
+        int i =start;
+        int j = end;
+
+        while(i<j){
+
+            //empty loop body
+            while(i<j && input[--j]>=pivot);
+
+            if (i<j){
+                input[i] = input[j];
+            }
+           //empty loop body
+            while(i<j && input[++i] <= pivot);
+
+            if (i<j){
+                input[j] = input[i];
+            }
+        }
+        input[j] = pivot;
+       return j;
+    }
+
 }
